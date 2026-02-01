2fe08830bde461542a0798eeda9427370462cf86
Bongani Ngcongolo <ngcongolobongani982@gmail.com>
2026-02-01 22:56:23 +0200

    Update app_profiler_menus.py

--- Project_/app_profiler_menus.py
+++ Project_/app_profiler_menus.py
@@ -37,31 +37,38 @@ for skill in skills:
 # ---------- EDUCATION ----------
 st.header("🎓 Education")
 st.write("""
-**[BSc(hons) Physics (UWC) - In progress]**  
-[BSc Physics (UWC) - Completed in 2025]  
+BSc(hons) Physics (UWC) - In progress
+BSc Physics (UWC) - Completed in 2025
 
 """)
 
 # ---------- PROJECTS ----------
 st.header("📂 Projects")
 
-st.write("**Project 1: [Project Name]**")
+st.write("**Project 1: Salary Data Analysis**")
 st.write("""
-Short description of the project.
-What problem it solves and what tools you used.
+Analysing the salary distribution for different departments to determine the business model used by the company to pay monthly salaries.
+This project focuses on determining the business model that is used in this company to fairly allocate monthly salaries to employees.
+There are no specific factors that the company has specified as their criteria in allocating the salaries, so we will carefully clean, 
+analyse, extract information, visualize trends and predict salaries.
 """)
 
-st.write("**Project 2: [Project Name]**")
+st.write("**Project 2:  product usage and user engagement evaluation")
 st.write("""
-Short description of the project.
+Conducted an end-to-end analysis of product usage and user engagement data to evaluate customer behavior, retention, and monetization patterns. 
+Cleaned and transformed messy event-level data, engineered time-based features, and handled missing values using group-level statistical 
+imputation. Analyzed session behavior, active user trends, revenue tiers, and engagement timing to identify high-value users and early churn 
+signals. Delivered actionable insights through exploratory data analysis, segmentation, and visualizations to support product and growth decision-making.
 """)
 
 # ---------- EXPERIENCE ----------
 st.header("💼 Experience")
 st.write("""
-**[Job/Internship/Volunteer Role]**  
-[Organization]  
-[What you did]
+**Second year mathematics tutor**  
+University of the Western cape
+     
+Job title, Second year Mathematics tutor at the University of the Western Cape responsible for Helping students with Linear Algebra, Algebraic structures, Advanced 
+calculus and Vector Calculus. I also helped with marking tutorials, assignmentsand verifying marks. 
 """)
 
 # ---------- CONTACT ----------
@@ -74,4 +81,5 @@ st.write("🐙 GitHub: https://github.com/BonganiNgcongolo")
 st.divider()
 
 st.write("© 2026 [Bongani Ngcongolo] — Built with Streamlit")
+
 
